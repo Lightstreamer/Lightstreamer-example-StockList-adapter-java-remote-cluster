d2b36d5abf1ae916204e5d194795d2393b557ab6
mone <simone.fabiano@gmail.com>
2014-09-24 11:31:02 +0200

    introduce the java-remote stocklist demo adapter

---
+++ src/stocklist_demo/adapters/StockQuotesDataAdapter.java
@@ -0,0 +1,190 @@
+/*
+*
+* Copyright 2013 Weswit s.r.l.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+package stocklist_demo.adapters;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import stocklist_demo.feed_simulator.ExternalFeedListener;
+import stocklist_demo.feed_simulator.ExternalFeedSimulator;
+import stocklist_demo.server.OutPrintLog;
+
+import com.lightstreamer.adapters.remote.DataProvider;
+import com.lightstreamer.adapters.remote.ItemEvent;
+import com.lightstreamer.adapters.remote.ItemEventListener;
+import com.lightstreamer.adapters.remote.SubscriptionException;
+import com.lightstreamer.adapters.remote.log.Logger;
+
+
+/**
+ * This Data Adapter accepts a limited set of item names (the names starting
+ * with "item") and listens to a (simulated) stock quotes feed, waiting for
+ * update events. The events pertaining to the currently subscribed items
+ * are then forwarded to Lightstreamer.
+ * This example demonstrates how a Data Adapter could interoperate with
+ * a broadcast feed (which always sends data for all available items)
+ * by selecting the updates to be sent. Many other types of feeds may exist,
+ * with very different behaviors.
+ */
+public class StockQuotesDataAdapter implements DataProvider {
+
+    private volatile ItemEventListener listener;
+    private final HashMap<String,Boolean> subscribedItems = new HashMap<String,Boolean>();
+    private final ExternalFeedSimulator myFeed;
+    
+    private Logger logger;
+    
+    /**
+     * Currently unused:
+     * A static map, to be used by the Metadata Adapter to find the data
+     * adapter instance; this allows the Metadata Adapter to forward client
+     * messages to the adapter.
+     * The map allows multiple instances of this Data Adapter to be included
+     * in different Adapter Sets. Each instance is identified with the name
+     * of the related Adapter Set; defining multiple instances in the same
+     * Adapter Set is not allowed.
+     */
+    public static final ConcurrentHashMap<String, StockQuotesDataAdapter> feedMap =
+        new ConcurrentHashMap<String, StockQuotesDataAdapter>();
+
+    public StockQuotesDataAdapter() {
+        myFeed = new ExternalFeedSimulator();
+    }
+
+    /**
+     * Starts the simulator feed (or connects to the external
+     * feed, for a real feed).
+     */
+    @Override
+    public void init(Map<String, String> params, String configFile) {
+        
+        logger = OutPrintLog.getInstance().getLogger("LS_demos_Logger.StockQuotes");
+        
+        //The feedMap of this adapter is never used
+        // Read the Adapter Set name, which is supplied by the Server as a parameter
+        String adapterSetId = (String) params.get("adapters_conf.id");
+        // Put a reference to this instance on a static map
+        // to be read by the Metadata Adapter
+        feedMap.put(adapterSetId, this);
+
+        myFeed.start();
+        logger.info("StockQuotesDataAdapter ready.");
+    }
+
+    @Override
+    public void setListener(ItemEventListener listener) {
+        this.listener = listener;
+        myFeed.setFeedListener(new MyFeedListener());
+    }
+
+    @Override
+    public void subscribe(String itemName) throws SubscriptionException {
+        logger.info("Subscribing to " + itemName);
+       
+        if (itemName.startsWith("item")) {
+            synchronized (subscribedItems) {
+                subscribedItems.put(itemName, new Boolean(false));
+            }
+            // now we ask the feed for the snapshot; our feed will insert
+            // an event with snapshot information into the normal updates flow
+            myFeed.sendCurrentValues(itemName);
+        } else {
+            logger.error("Cannot subscribe to " + itemName + " - only names starting with \"item\" are supported");
+            throw new SubscriptionException("Unexpected item: " + itemName);
+        }
+    }
+
+    @Override
+    public void unsubscribe(String itemName) {
+        logger.info("Unsubscribing from " + itemName);
+        synchronized (subscribedItems) {
+            subscribedItems.remove(itemName);
+        }
+    }
+
+    @Override
+    public boolean isSnapshotAvailable(String itemName) {
+        return true;
+    }
+
+    public void clearStatus() {
+        synchronized (subscribedItems) {
+            Set<String> keys = subscribedItems.keySet();
+            for (String itemName : keys) {
+                listener.clearSnapshot(itemName);
+            }
+        }
+    }
+    
+    private class MyFeedListener implements ExternalFeedListener {
+
+        /**
+         * Called by our feed for each update event occurrence on some stock.
+         * If isSnapshot is true, then the event contains a full snapshot
+         * with the current values of all fields for the stock.
+         */
+        
+        public void onEvent(String itemName, final HashMap<String,String> currentValues,
+                            boolean isSnapshot) {
+            synchronized (subscribedItems) {
+                if (!subscribedItems.containsKey(itemName)) {
+                    return;
+                }
+                Boolean started = (Boolean) subscribedItems.get(itemName);
+                boolean snapshotReceived = started.booleanValue();
+                if (!snapshotReceived) {
+                    if (!isSnapshot) {
+                        // we ignore the update and keep waiting until
+                        // a full snapshot for the item has been received
+                        return;
+                    }
+                    subscribedItems.put(itemName, new Boolean(true));
+                } else {
+                    if (isSnapshot) {
+                        // it's not the first event we have received carrying
+                        // snapshot information for the item; so, this event
+                        // is not a snapshot from Lightstreamer point of view
+                        isSnapshot = false;
+                    }
+                }
+
+               
+                listener.update(itemName, new ItemEvent() {
+                    
+                    public final Iterator<String> getNames() {
+                        return currentValues.keySet().iterator();
+                    }
+
+                    public final Object getValue(String name) {
+                        return currentValues.get(name);
+                    }
+                
+                }, isSnapshot);
+                
+                            
+            }
+
+        }
+
+    }
+    
+}
---
+++ src/stocklist_demo/server/ServerMain.java
@@ -0,0 +1,167 @@
+package stocklist_demo.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import stocklist_demo.adapters.StockQuotesDataAdapter;
+
+import com.lightstreamer.adapters.remote.DataProviderServer;
+import com.lightstreamer.adapters.remote.LiteralBasedProvider;
+import com.lightstreamer.adapters.remote.MetadataProviderServer;
+import com.lightstreamer.adapters.remote.Server;
+import com.lightstreamer.adapters.remote.log.Logger;
+
+/*
+ * Copyright 2013 Weswit Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+public class ServerMain {
+    private static Logger _log = OutPrintLog.getInstance().getLogger("LS_demos_Logger.StockQuotes.Server");
+
+    public static final String PREFIX1 = "-";
+    public static final String PREFIX2 = "/";
+
+    public static final char SEP = '=';
+
+    public static final String ARG_HELP_LONG = "help";
+    public static final String ARG_HELP_SHORT = "?";
+
+    public static final String ARG_HOST = "host";
+    public static final String ARG_METADATA_RR_PORT = "metadata_rrport";
+    public static final String ARG_DATA_RR_PORT = "data_rrport";
+    public static final String ARG_DATA_NOTIF_PORT = "data_notifport";
+    public static final String ARG_NAME = "name";
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            help();
+        }
+
+        _log.info("Lightstreamer StockListDemo .NET Adapter Standalone Server starting...");
+
+        Server.setLoggerProvider(OutPrintLog.getInstance());
+
+        Map<String,String> parameters = new HashMap<String,String>();
+        String host = null;
+        int rrPortMD = -1;
+        int rrPortD = -1;
+        int notifPortD = -1;
+        String name = null;
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+            if (arg.startsWith(PREFIX1) || arg.startsWith(PREFIX2)) {
+                arg = arg.substring(1).toLowerCase();
+
+                if (arg.equals(ARG_HELP_SHORT) || arg.equals(ARG_HELP_LONG)) {
+                    help();
+                } else if (arg.equals(ARG_HOST)) {
+                    i++;
+                    host = args[i];
+
+                    _log.debug("Found argument: '" + ARG_HOST + "' with value: '" + host + "'");
+                } else if (arg.equals(ARG_METADATA_RR_PORT)) {
+                    i++;
+                    rrPortMD = Integer.parseInt(args[i]);
+
+                    _log.debug("Found argument: '" + ARG_METADATA_RR_PORT + "' with value: '" + rrPortMD + "'");
+                } else if (arg.equals(ARG_DATA_RR_PORT)) {
+                    i++;
+                    rrPortD = Integer.parseInt(args[i]);
+
+                    _log.debug("Found argument: '" + ARG_DATA_RR_PORT + "' with value: '" + rrPortD + "'");
+                } else if (arg.equals(ARG_DATA_NOTIF_PORT)) {
+                    i++;
+                    notifPortD = Integer.parseInt(args[i]);
+
+                    _log.debug("Found argument: '" + ARG_DATA_NOTIF_PORT + "' with value: '" + notifPortD + "'");
+                } else if (arg.equals(ARG_NAME)) {
+                    i++;
+                    name = args[i];
+
+                    _log.debug("Found argument: '" + ARG_NAME + "' with value: '" + name + "'");
+                }
+
+            } else {
+                int sep = arg.indexOf(SEP);
+                if (sep < 1) {
+                    _log.warn("Skipping unrecognizable argument: '" + arg + "'");
+
+                } else {
+                    String par = arg.substring(0, sep).trim();
+                    String val = arg.substring(sep + 1).trim();
+                    parameters.put(par, val);
+
+                    _log.debug("Found parameter: '" + par + "' with value: '" + val + "'");
+                }
+            }
+        }
+
+        {
+            MetadataProviderServer server = new MetadataProviderServer();
+            server.setAdapter(new LiteralBasedProvider());
+            server.setAdapterParams(parameters);
+            // server.setAdapterConfig not needed by LiteralBasedProvider
+            if (name != null) {
+                server.setName(name);
+            }
+            _log.debug("Remote Metadata Adapter initialized");
+   
+            ServerStarter starter = new ServerStarter(host, rrPortMD, -1);
+            starter.launch(server);
+        }
+        {
+            DataProviderServer server = new DataProviderServer();
+            server.setAdapter(new StockQuotesDataAdapter());
+            // server.AdapterParams not needed by StockListDemoAdapter
+            // server.AdapterConfig not needed by StockListDemoAdapter
+            if (name != null) {
+                server.setName(name);
+            }
+            _log.debug("Remote Data Adapter initialized");
+   
+            ServerStarter starter = new ServerStarter(host, rrPortD, notifPortD);
+            starter.launch(server);
+        }
+
+        _log.info("Lightstreamer StockListDemo .NET Adapter Standalone Server running");
+    }
+
+    private static void help() {
+        _log.fatal("Lightstreamer StockListDemo .NET Adapter Standalone Server Help");
+        _log.fatal("Usage: ");
+        _log.fatal("                     [-name <name>] -host <address>");
+        _log.fatal("                     -metadata_rrport <port> -data_rrport <port> -data_notifport <port>");
+        _log.fatal("                     [\"<param1>=<value1>\" ... \"<paramN>=<valueN>\"]");
+        _log.fatal("Where: <name>        is the symbolic name for both the adapters (1)");
+        _log.fatal("       <address>     is the host name or ip address of LS server (2)");
+        _log.fatal("       <port>        is the tcp port number where LS proxy is listening on (3)");
+        _log.fatal("       <paramN>      is the Nth metadata adapter parameter name (4)");
+        _log.fatal("       <valueN>      is the value of the Nth metadata adapter parameter (4)");
+        _log.fatal("Notes: (1) The adapter name is optional, if it is not given the adapter will be");
+        _log.fatal("           assigned a progressive number name like \"#1\", \"#2\" and so on");
+        _log.fatal("       (2) The communication will be from here to LS, not viceversa");
+        _log.fatal("       (3) The notification port is necessary for a Data Adapter, while it is");
+        _log.fatal("           not needed for a Metadata Adapter");
+        _log.fatal("       (4) The parameters name/value pairs will be passed to the LiteralBasedProvider");
+        _log.fatal("           Metadata Adapter as a Map in the \"parameters\" Init() argument");
+        _log.fatal("           The StockListDemo Data Adapter requires no parameters");
+        _log.fatal("Aborting...");
+
+        System.exit(9);
+    }
+}
