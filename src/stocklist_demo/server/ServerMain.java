7ceb4b4c6a3a00511d840793956ff3cdb63742f6
mone <simone.fabiano@gmail.com>
2014-09-24 11:34:56 +0200

    update licenses

--- src/stocklist_demo/server/ServerMain.java
+++ src/stocklist_demo/server/ServerMain.java
@@ -1,3 +1,21 @@
+/*
+*
+* Copyright 2014 Weswit s.r.l.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 package stocklist_demo.server;
 
 import java.util.HashMap;
@@ -10,23 +28,6 @@ import com.lightstreamer.adapters.remote.LiteralBasedProvider;
 import com.lightstreamer.adapters.remote.MetadataProviderServer;
 import com.lightstreamer.adapters.remote.Server;
 import com.lightstreamer.adapters.remote.log.Logger;
-
-/*
- * Copyright 2013 Weswit Srl
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 public class ServerMain {
     private static Logger _log = OutPrintLog.getInstance().getLogger("LS_demos_Logger.StockQuotes.Server");
